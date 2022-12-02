9f694d893fdc5f62ae7c0f08a0470922a3a7a4aa
cmurph03 <110257614+cmurph03@users.noreply.github.com>
2022-12-02 08:49:08 -0600

    Add files via upload

---
+++ Lab3/energySricpt.m
@@ -0,0 +1,18 @@
+%-------------------------------------------------------------
+% Created: 9/3/2021 by Caroline Murphy
+%
+% Revision History: None
+%
+% Purpose: This script is used to generate a singal to test the functions for
+% Lab 3
+%
+% Variables created: 
+%   t0  - center of the pi-function
+%   tau - width of the pi-function
+%   t   - time vector
+%   x   - samples vector of the pi-function
+%-------------------------------------------------------------
+
+disp('Generating Signal to Test Lab 3 Functions')
+
+[t,x,a,b] = mysignal(2);
